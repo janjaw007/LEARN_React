72db3502c2dedc69debe94dd766e63abe2549cfd
janjaw007 <chayanit.kitm@gmail.com>
2023-09-09 23:09:18 +0700

    end section10 start ch10

---
+++ 00-challenge/my-app/src/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <react.StrictMode>
+    <App />
+  </react.StrictMode>
+);
